e777f18ff94515048adc8098d029aa473b2e94e9
NA YEON LEE <lee.nayeon90@gmail.com>
2018-12-17 17:52:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+const driver = {
+};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    const newDriver = {...driver};
+    newDriver[key] = value;
+    return newDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+}
+
+function deleteFromDriverByKey(driver, key) {
+  const newDriver = {...driver};
+  delete newDriver.key;
+  return newDriver;
+  }
+  
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  const newDriver = {...driver};
+  delete newDriver.key;
+  return newDriver;
+  }
+
